c9278a40bac102144d665869085c52a12fe2fb74
s9gf4ult <s9gf4ult@gmail.com>
2013-01-08 03:12:05 +0600

    modified:   conduplay.cabal 	modified:   conduplay.hs

--- conduplay.hs
+++ conduplay.hs
@@ -2,12 +2,13 @@ module Main where
 
 import Data.Conduit
 import Data.Conduit.Binary
-import Data.Conduit.List as CL
+import qualified Data.Conduit.List as CL
 import Data.Conduit.Text
 import Data.Text as T
-import Data.HashMap.Strict as M
+import Data.Map as M
 import Data.Set as S
 import Data.Char as C
+import Data.Monoid
 
 import Control.Monad
 import Control.Monad.Trans
@@ -15,28 +16,56 @@ import System.Environment
 import Debug.Trace 
 
 main = do
-    [fp] <- getArgs 
-    runResourceT $ 
-        sourceFile fp $= decode utf8
-                      $= CL.map (T.filter isvalid)
-                      $$ byN 3 =$= collect M.empty 
-                      =$ (await >>= lift . lift . print)
+    args <- getArgs
+    case args of
+      [fp] -> do
+        runResourceT $ 
+          sourceFile fp $= decode utf8
+          $= CL.map (T.filter isvalid)
+          $$ byN 3 =$= collect M.empty 
+          =$ (await >>= lift . lift . print)
     where
       isvalid c = S.member (C.toLower c) elems
 
       elems = S.fromList $ ['a'..'z'] ++ ['а'..'я'] ++ " ,.!?-"
       
-      byN n = do
-        mx <- await
-        case mx of 
-          Nothing -> return ()
-          Just x  -> 
-            let t' = T.take n x
-            in do
-              yield t'
-              when (T.length x > 1) $ do
-                leftover $ T.drop 1 x
-                byN n
+      byN count = byN' T.empty count
+
+      byN' t count
+        | l >= count = do
+          yield $ T.take count t
+          byN' (T.drop count t) count
+        | l > 0 = do
+          a <- await
+          case a of
+            Nothing -> do
+              yield t
+              return ()
+            Just b -> let
+              d = count - l
+              in do
+                yield $ mappend t $ T.take d b
+                let (res, rest) = yieldfrom (T.drop d b)
+                mapM yield res
+                byN' rest count
+        | otherwise = do
+          a <- await
+          case a of
+            Nothing -> return ()
+            Just b -> do
+              let (res, rest) = yieldfrom b
+              mapM yield res
+              byN' rest count
+
+        where
+          l = T.length t
+          yieldfrom txt | T.length txt < count = ([], txt)
+                        | otherwise = (bfr:more, rest)
+            where
+              (bfr, aftr) = T.splitAt count txt
+              (more, rest) = yieldfrom aftr
+        
+          
     
       collect e = do
         mx <- await
